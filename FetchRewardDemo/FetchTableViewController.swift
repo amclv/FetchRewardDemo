a471aaa304d9181fc93e7cae83843e89af23bc47
Aaron Cleveland <toscleveland@gmail.com>
2020-09-10 11:38:57 -0500

    Init Project / Programmatically

---
+++ FetchRewardDemo/FetchTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FetchTableViewController.swift
+//  FetchRewardDemo
+//
+//  Created by Aaron Cleveland on 9/10/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class FetchTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
